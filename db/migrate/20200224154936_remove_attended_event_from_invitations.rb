b7882055af9da82f700f00ef4c187652f7371c4a
JFAldridge <jfaldridge1985@gmail.com>
2020-03-06 10:41:39 -0500

    More migration fixes

--- db/migrate/20200224154936_remove_attended_event_from_invitations.rb
+++ db/migrate/20200224154936_remove_attended_event_from_invitations.rb
@@ -1,5 +1,5 @@
 class RemoveAttendedEventFromInvitations < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :invitations, :attended_event, foreign_key: true
+    remove_reference :invitations, :attended_event
   end
 end
--- db/migrate/20200224155221_remove_invitee_from_invitations.rb
+++ db/migrate/20200224155221_remove_invitee_from_invitations.rb
@@ -1,5 +1,5 @@
 class RemoveInviteeFromInvitations < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :invitations, :invitee, foreign_key: true
+    remove_reference :invitations, :invitee
   end
 end
